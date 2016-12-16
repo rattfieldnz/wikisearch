04e640cad1f71c296b9298a9c7d769736cb654e8
Rob Attfield <emailme@robertattfield.com>
2016-12-16 23:06:05 +1300

    Preparing connection wi Wikipedia API, set-up BackboneJS collection with API info.

--- app/scripts/collections/wikis.js
+++ app/scripts/collections/wikis.js
@@ -1,4 +1,22 @@
 var Wikis = Backbone.Collection.extend({
 
-    initialize: function(options) {}
+    initialize: function(options){
+        if (options.keywords){
+            this.keywords = options.keywords;
+        }
+    },
+
+    url: function()
+    {
+        return "https://en.wikipedia.org/w/api.php?" +
+            "format=json&action=query&generator=search" +
+            "&gsrnamespace=0&gsrlimit=&prop=pageimages|extracts" +
+            "&pilimit=max&exintro&explaintext&exsentences=1" +
+            "&exlimit=max&gsrsearch="+this.keywords;
+    },
+
+    parse: function(response)
+    {
+        return response.query;
+    }
 }); 
--- app/scripts/views/search.js
+++ app/scripts/views/search.js
@@ -2,9 +2,25 @@ WikiApp.Views.Search = Backbone.View.extend({
 
     initialize: function(options) {},
 
+    template: "<input type='text' placeholder='search'> \
+               <button>Search</button> \
+               <ul id='wiki-list'></ul>",
+
+    getWikis: function() {
+
+        var keywords = this.$el.find('input').val();
+        var wikis = new WikiApp.Collections.Wikis({keywords: keywords});
+
+        wikis.fetch();
+    },
+
+    events: {
+        'click button' : 'getWikis'
+    },
+
     render: function()
     {
-        this.$el.html("This is my search page!!");
+        this.$el.html(this.template);
         return this;
     }
 });
